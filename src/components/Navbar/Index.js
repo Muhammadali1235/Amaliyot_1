0339a429c71096a2f1a1dc48d08e3d6a46b7100e
Ali_developer <rahimjonovmuhammadali1@gmail.com>
2023-09-13 18:09:58 +0500

    added components

---
+++ src/components/Navbar/Index.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+function Index() {
+    return (
+        <div>
+            <div className="navbar">
+                <div className="logo">
+                    <h1>LOGO</h1>
+                </div>
+                <ul>
+                    <li>
+                        <a href="#">Home</a>
+                    </li>
+                    <li>
+                        <a href="#">About</a>
+                    </li>
+                    <li>
+                        <a href="#">Setting</a>
+                    </li>
+                    <li>
+                        <a href="#">Conuntin</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+export default Index
